fa24fa69fbad82671694a2872391e90de4e34511
Walden1995 <sew@jdwalden.com>
2017-09-21 15:07:02 -0500

    fixed formatting of aboutme.html

---
+++ temp.js
@@ -0,0 +1,98 @@
+var contents = [
+  {
+    "name": "index.html",
+    "path": "index.html",
+    "sha": "15a75565243dcf2abf273d2ef257279429250b8b",
+    "size": 2673,
+    "url": "https://api.github.com/repos/Walden1995/IT202/contents/index.html?ref=master",
+    "html_url": "https://github.com/Walden1995/IT202/blob/master/index.html",
+    "git_url": "https://api.github.com/repos/Walden1995/IT202/git/blobs/15a75565243dcf2abf273d2ef257279429250b8b",
+    "download_url": "https://raw.githubusercontent.com/Walden1995/IT202/master/index.html",
+    "type": "file",
+    "_links": {
+      "self": "https://api.github.com/repos/Walden1995/IT202/contents/index.html?ref=master",
+      "git": "https://api.github.com/repos/Walden1995/IT202/git/blobs/15a75565243dcf2abf273d2ef257279429250b8b",
+      "html": "https://github.com/Walden1995/IT202/blob/master/index.html"
+    }
+  },
+  {
+    "name": "lab1",
+    "path": "lab1",
+    "sha": "80755f5dd9b86523eafb985fb5db737dadc26ca0",
+    "size": 0,
+    "url": "https://api.github.com/repos/Walden1995/IT202/contents/lab1?ref=master",
+    "html_url": "https://github.com/Walden1995/IT202/tree/master/lab1",
+    "git_url": "https://api.github.com/repos/Walden1995/IT202/git/trees/80755f5dd9b86523eafb985fb5db737dadc26ca0",
+    "download_url": null,
+    "type": "dir",
+    "_links": {
+      "self": "https://api.github.com/repos/Walden1995/IT202/contents/lab1?ref=master",
+      "git": "https://api.github.com/repos/Walden1995/IT202/git/trees/80755f5dd9b86523eafb985fb5db737dadc26ca0",
+      "html": "https://github.com/Walden1995/IT202/tree/master/lab1"
+    }
+  },
+  {
+    "name": "lab2",
+    "path": "lab2",
+    "sha": "acbe0c0436bb7998798c481018d02856183d0068",
+    "size": 0,
+    "url": "https://api.github.com/repos/Walden1995/IT202/contents/lab2?ref=master",
+    "html_url": "https://github.com/Walden1995/IT202/tree/master/lab2",
+    "git_url": "https://api.github.com/repos/Walden1995/IT202/git/trees/acbe0c0436bb7998798c481018d02856183d0068",
+    "download_url": null,
+    "type": "dir",
+    "_links": {
+      "self": "https://api.github.com/repos/Walden1995/IT202/contents/lab2?ref=master",
+      "git": "https://api.github.com/repos/Walden1995/IT202/git/trees/acbe0c0436bb7998798c481018d02856183d0068",
+      "html": "https://github.com/Walden1995/IT202/tree/master/lab2"
+    }
+  },
+  {
+    "name": "list.css",
+    "path": "list.css",
+    "sha": "90223c02b2918b5a7d6899eda6dff0b97d683293",
+    "size": 4697,
+    "url": "https://api.github.com/repos/Walden1995/IT202/contents/list.css?ref=master",
+    "html_url": "https://github.com/Walden1995/IT202/blob/master/list.css",
+    "git_url": "https://api.github.com/repos/Walden1995/IT202/git/blobs/90223c02b2918b5a7d6899eda6dff0b97d683293",
+    "download_url": "https://raw.githubusercontent.com/Walden1995/IT202/master/list.css",
+    "type": "file",
+    "_links": {
+      "self": "https://api.github.com/repos/Walden1995/IT202/contents/list.css?ref=master",
+      "git": "https://api.github.com/repos/Walden1995/IT202/git/blobs/90223c02b2918b5a7d6899eda6dff0b97d683293",
+      "html": "https://github.com/Walden1995/IT202/blob/master/list.css"
+    }
+  },
+  {
+    "name": "project1",
+    "path": "project1",
+    "sha": "9df2f039a633e43883ecbf092405109d81a9f508",
+    "size": 0,
+    "url": "https://api.github.com/repos/Walden1995/IT202/contents/project1?ref=master",
+    "html_url": "https://github.com/Walden1995/IT202/tree/master/project1",
+    "git_url": "https://api.github.com/repos/Walden1995/IT202/git/trees/9df2f039a633e43883ecbf092405109d81a9f508",
+    "download_url": null,
+    "type": "dir",
+    "_links": {
+      "self": "https://api.github.com/repos/Walden1995/IT202/contents/project1?ref=master",
+      "git": "https://api.github.com/repos/Walden1995/IT202/git/trees/9df2f039a633e43883ecbf092405109d81a9f508",
+      "html": "https://github.com/Walden1995/IT202/tree/master/project1"
+    }
+  },
+  {
+    "name": "project2",
+    "path": "project2",
+    "sha": "d89a9f38ca833871920ccd9bb14e75fbd80e0213",
+    "size": 0,
+    "url": "https://api.github.com/repos/Walden1995/IT202/contents/project2?ref=master",
+    "html_url": "https://github.com/Walden1995/IT202/tree/master/project2",
+    "git_url": "https://api.github.com/repos/Walden1995/IT202/git/trees/d89a9f38ca833871920ccd9bb14e75fbd80e0213",
+    "download_url": null,
+    "type": "dir",
+    "_links": {
+      "self": "https://api.github.com/repos/Walden1995/IT202/contents/project2?ref=master",
+      "git": "https://api.github.com/repos/Walden1995/IT202/git/trees/d89a9f38ca833871920ccd9bb14e75fbd80e0213",
+      "html": "https://github.com/Walden1995/IT202/tree/master/project2"
+    }
+  }
+]
