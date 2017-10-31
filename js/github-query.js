009082223dd81e445c307357e9414186c2fbcacc
Stephen Walden <swalde3@uic.edu>
2017-10-31 15:35:04 +0000

    better dynamics for education modal

--- js/github-query.js
+++ js/github-query.js
@@ -5,12 +5,27 @@ jQuery.githubContent = function(username, repo, callback) {
 function loadContent() {
     $.get("folders.JSON",function(data){
         $.each(data.data, function() {
-            var node = $(".sample").clone();
+            var node = $("#listitem").clone();
             node.removeClass("sample");
+            node.attr('id','added');
             node.find("A").attr("href",this.link);
             node.find("STRONG").text(this.name);
             $("#list").append(node); 
         });
+    });
+}
+
+function loadModal(){
+    $.get("education.JSON",function(data){
+       $.each(data.data, function(){
+           var node = $('#educationCard').clone();
+           node.removeClass('sample');
+           node.attr('id','added');
+           node.find('img').attr('src',this.link);
+           node.find('H1').text(this.name);
+           node.find('P').text(this.info);
+           $('#education').prepend(node);
+       }) ;
     });
 }
     
