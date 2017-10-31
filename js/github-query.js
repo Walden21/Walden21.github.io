b3da9b8e8ea90eb5258659f6d92b67b5755e3716
Walden1995 <sew@jdwalden.com>
2017-10-30 23:20:06 -0500

    Re-Developed website; Fixed Map on Modal issues; Moved away from Agency website;

--- js/github-query.js
+++ js/github-query.js
@@ -1,46 +1,17 @@
-jQuery.githubUser = function(username, callback) {
-   jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
-}
-
-jQuery.fn.loadRepositories = function(username) {
-    
-
-    $.githubUser(username, function(data) {
-        var repos = data.data; // JSON Parsing
-         
-
-        $(repos).each(function() {
-            if (this.name != (username.toLowerCase()+'.github.com') && this.name != "Walden1995.github.io"  && this.description != null) {
-                var node = $("#sample").clone();
-                node.find("A").attr("href","https://Walden1995.github.io/"+(this.name) + "/");
-                node.find("STRONG").text((this.name).substring(0,8));
-                node.find("P").text(this.description);
-                $("#repo-listing").append(node);
-            }
-        });      
-     });
-};
-
 jQuery.githubContent = function(username, repo, callback) {
    jQuery.getJSON('https://api.github.com/repos/'+username+'/'+repo+'/contents?callback=?',callback)
 }
 
- 
-jQuery.fn.loadRepoContent = function(username, repo) {
-    $.githubContent(username, repo, function(data) {
-        var repos = data.data; // JSON Parsing
-
-        $(repos).each(function() {
-            if (this.type == "dir") {
-                
-							var node = $("#listitem").clone();
-							
-							node.find("A").attr("href",(this.name) + "/");
-							node.find("STRONG").text(this.name.substring(0,8));
-
-							$("#list").append(node);
-		
-            }
-        });      
-      });
-};
+function loadContent() {
+    $.get("folders.JSON",function(data){
+        $.each(data.data, function() {
+            var node = $(".sample").clone();
+            node.removeClass("sample");
+            node.find("A").attr("href",this.link);
+            node.find("STRONG").text(this.name);
+            $("#list").append(node); 
+        });
+    });
+}
+    
+        
