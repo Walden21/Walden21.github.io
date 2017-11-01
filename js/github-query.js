fc5de26342d783a235d01f21421ed656031b20cd
Stephen Walden <swalde3@uic.edu>
2017-11-01 16:56:58 +0000

    made various fixes

--- js/github-query.js
+++ js/github-query.js
@@ -1,43 +1,34 @@
-jQuery.githubContent = function(username, repo, callback) {
-   jQuery.getJSON('https://api.github.com/repos/'+username+'/'+repo+'/contents?callback=?',callback)
-}
-
 function loadContent() {
-    $.get("folders.JSON",function(data){
+    $.get("folders.JSON", function(data) {
         $.each(data.data, function() {
             var node = $("#listitem").clone();
             node.removeClass("sample");
-            node.attr('id','added');
-            node.find("A").attr("href",this.link);
+            node.attr('id', 'added');
+            node.find("A").attr("href", this.link);
             node.find("STRONG").text(this.name);
-            $("#list").append(node); 
+            $("#list").append(node);
         });
     });
 }
 
-function loadModal(){
-    $.get("education.JSON",function(data){
-       $.each(data.data, function(){
-           var node = $('#educationCard').clone();
-           node.removeClass('sample');
-           node.attr('id','added');
-           node.find('img').attr('src',this.link);
-           node.find('SPAN').text(this.name);
-           node.find('P').text(this.info);
-           $('#education').prepend(node);
-       });
-    });
-    $.get("hobbies.JSON",function(data){
-      $.each(data.data,function(){
-        var node = $('#hobbyCard').clone();
-        node.removeClass('sample');
-        node.attr('id','added');
-        node.find('img').attr('src',this.link);
-        node.find('SPAN').text(this.name);
-        node.find('P').text(this.info);
-        $('#hobbies').prepend(node);
-      });  
+function loadTimeline(json, card, list) {
+    $.get(json, function(data) {
+        $.each(data.data, function() {
+            var node = card.clone();
+            node.removeClass('sample');
+            node.attr('id', 'added');
+            node.find('img').attr('src', this.link);
+            node.find('H4').text(this.name);
+            node.find('P').text(this.info);
+            list.append(node);
+        });
+        var node = $('#timelineBubble').clone();
+        node.attr('id', 'added');
+        list.append(node);
     });
 }
-    
-        
+
+function loadModal() {
+    loadTimeline("education.JSON",$('#educationCard'),$('#education'));
+    loadTimeline("hobbies.JSON",$('#hobbyCard'),$('#hobbies'));
+}
